35574b4fc56f4972afc319932e7c75d0ba2e2485
Almonther Alwaili <montherwaili@gmail.com>
2026-08-30 11:02:24 +0400

    create class PhoneNumberService

---
+++ demo/demo/src/main/java/com/cl/demo/services/PhoneNumberService.java
@@ -0,0 +1,15 @@
+package com.cl.demo.services;
+
+import com.cl.demo.DemoApplication;
+import com.cl.demo.entities.PhoneNumber;
+import com.cl.demo.requestobjects.PhoneNumberCreateRequest;
+import com.cl.demo.requestobjects.PhoneNumberUpdateRequest;
+import com.cl.demo.HelperUtils;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
+public class PhoneNumberService {
+
+    }
+}
