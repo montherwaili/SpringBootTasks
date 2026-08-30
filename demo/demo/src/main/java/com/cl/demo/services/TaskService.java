7a8701460e440855b48e5fe0cddbc7fc22797f81
Almonther Alwaili <montherwaili@gmail.com>
2026-08-30 09:24:36 +0400

    create class TaskService

---
+++ demo/demo/src/main/java/com/cl/demo/services/TaskService.java
@@ -0,0 +1,15 @@
+package com.cl.demo.services;
+
+import com.cl.demo.DemoApplication;
+import com.cl.demo.entities.Task;
+import com.cl.demo.requestobjects.TaskCreateRequest;
+import com.cl.demo.requestobjects.TaskUpdateRequest;
+import com.cl.demo.HelperUtils;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
+public class TaskService {
+    public static final String TASK_SAVED = "Task saved";
+    }
+}
