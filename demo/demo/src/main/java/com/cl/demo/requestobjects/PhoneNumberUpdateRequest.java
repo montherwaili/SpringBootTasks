58d567eccfcefe22b627e4d9684defdbc40b6fde
Almonther Alwaili <montherwaili@gmail.com>
2026-08-30 09:58:43 +0400

    fill in

--- demo/demo/src/main/java/com/cl/demo/requestobjects/PhoneNumberUpdateRequest.java
+++ demo/demo/src/main/java/com/cl/demo/requestobjects/PhoneNumberUpdateRequest.java
@@ -1,4 +1,13 @@
 package com.cl.demo.requestobjects;
 
+import lombok.Getter;
+import lombok.Setter;
+import java.util.UUID;
+
+@Getter
+@Setter
 public class PhoneNumberUpdateRequest {
+    private UUID uuid;
+    private String countryCodeToUpdate;
+    private Long phoneNumberToUpdate;
 }
