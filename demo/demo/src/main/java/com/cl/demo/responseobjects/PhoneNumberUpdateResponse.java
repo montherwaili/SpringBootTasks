f002a21342397f4156d7d3a927a3d84a278a7e06
Almonther Alwaili <montherwaili@gmail.com>
2026-08-30 10:50:42 +0400

    create class PhoneNumberUpdateResponse

---
+++ demo/demo/src/main/java/com/cl/demo/responseobjects/PhoneNumberUpdateResponse.java
@@ -0,0 +1,16 @@
+package com.cl.demo.responseobjects;
+
+import com.cl.demo.entities.PhoneNumber;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class PhoneNumberUpdateResponse {
+    private String phoneNumberId;
+    private String countryCode;
+    private Long phoneNumber;
+    }
+}
