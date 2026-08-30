2bd3a28a818c3388050de8ccc741d93f1b2be964
Almonther Alwaili <montherwaili@gmail.com>
2026-08-30 09:30:13 +0400

    addTask(...) + costructor

--- demo/demo/src/main/java/com/cl/demo/services/TaskService.java
+++ demo/demo/src/main/java/com/cl/demo/services/TaskService.java
@@ -11,5 +11,36 @@ import java.util.*;
 @Service
 public class TaskService {
     public static final String TASK_SAVED = "Task saved";
+
+
+    public Map<String, String> addTask(TaskCreateRequest req) {
+        Map<String, String> response = new HashMap<>();
+
+
+        if (req.getTitle() == null || req.getTitle().trim().isEmpty()) {
+            response.put("error", "Title cannot be empty");
+            return response;
+        }
+
+        Task task = new Task();
+        task.setId(UUID.randomUUID());
+        task.setIsActive(Boolean.TRUE);
+        task.setCreatedDate(new Date());
+        task.setTaskNumber(generateTaskNumber());
+
+
+        task.setTitle(req.getTitle());
+        task.setDescription(req.getDescription());
+        task.setDueDate(req.getDueDate());
+        task.setStartDate(req.getStartDate());
+        task.setTaskStatus(req.getTaskStatus());
+        task.setIsAssigned(req.getIsAssigned());
+
+
+        if (DemoApplication.Task_List.add(task)) {
+            response.put("response", TASK_SAVED);
+        }
+        return response;
+    }
     }
 }
