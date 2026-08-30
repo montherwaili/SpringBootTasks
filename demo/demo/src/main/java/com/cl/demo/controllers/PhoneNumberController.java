1d8cb5d02208d23c2474b827d095137773976161
Almonther Alwaili <montherwaili@gmail.com>
2026-08-30 14:38:53 +0400

    create class  PhoneNumberController

---
+++ demo/demo/src/main/java/com/cl/demo/controllers/PhoneNumberController.java
@@ -0,0 +1,21 @@
+package com.cl.demo.controllers;
+
+import com.cl.demo.entities.PhoneNumber;
+import com.cl.demo.requestobjects.PhoneNumberCreateRequest;
+import com.cl.demo.requestobjects.PhoneNumberUpdateRequest;
+import com.cl.demo.responseobjects.PhoneNumberCreateResponse;
+import com.cl.demo.responseobjects.PhoneNumberUpdateResponse;
+import com.cl.demo.services.PhoneNumberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("phoneNumber")
+public class PhoneNumberController {
+
+    @Autowired
+    private PhoneNumberService phoneNumberService;
+
+
