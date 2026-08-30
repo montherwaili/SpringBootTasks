6f58c0b9da94c1193f28b28245a6db14f2ecef3f
Almonther Alwaili <montherwaili@gmail.com>
2026-08-30 09:57:48 +0400

    fill in

--- demo/demo/src/main/java/com/cl/demo/requestobjects/PhoneNumberCreateRequest.java
+++ demo/demo/src/main/java/com/cl/demo/requestobjects/PhoneNumberCreateRequest.java
@@ -1,4 +1,11 @@
 package com.cl.demo.requestobjects;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PhoneNumberCreateRequest {
+    private String countryCode;
+    private Long phoneNumber;
 }
